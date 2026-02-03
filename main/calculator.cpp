0e1798f2e7c4a31755becec769d36b9099a92829
Noppasin kumto <noppasin936@gmail.com>
2026-02-03 23:47:19 +0700

    new feature

--- main/calculator.cpp
+++ main/calculator.cpp
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    
+    printf("Calculator : ");
+        scanf("%d", &result);
+
+        while(1) {
+            int c = getchar();
+
+            if(c == '\n') {
+                break;
+            }
+
+            op = c;
+
+            scanf("%d", &num);
+
+
+        }
 }
